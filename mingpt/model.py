b53e8207b2b51451eac4bfc882b2c142655149dc
Mehrdad Yazdani <myazdani@gmail.com>
2022-07-02 20:11:58 -0700

    double checked why we need transpose and putting it back

--- mingpt/model.py
+++ mingpt/model.py
@@ -72,11 +72,8 @@ class CausalSelfAttention(nn.Module):
         att = att.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))
         att = F.softmax(att, dim=-1)
         att = self.attn_drop(att)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        #y = torch.einsum('ijkl,ijlm->ijkm',[att, v]) # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        #y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-        y = y.contiguous().view(B, T, C) # re-assemble all head outputs side by side
-
+        y = torch.einsum('ijkl,ijlm->ijkm',[att, v]) # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
         # output projection
         y = self.resid_drop(self.proj(y))
         return y
