f085097cbe2a8404f498764b1bf9e23c5bbe1859
Mehrdad Yazdani <myazdani@gmail.com>
2022-07-02 19:37:56 -0700

    try einsum again

--- mingpt/model.py
+++ mingpt/model.py
@@ -71,7 +71,8 @@ class CausalSelfAttention(nn.Module):
         att = att.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))
         att = F.softmax(att, dim=-1)
         att = self.attn_drop(att)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        #y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = torch.einsum('ijkl,ijkm->ijkm',[att, v]) # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
         # output projection
