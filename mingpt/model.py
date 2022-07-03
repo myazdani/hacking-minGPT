6016b3be3fa9c8788356c15a3065edb3bef2bb67
Mehrdad Yazdani <myazdani@gmail.com>
2022-07-02 20:05:52 -0700

    put simpler reshape back

--- mingpt/model.py
+++ mingpt/model.py
@@ -74,7 +74,8 @@ class CausalSelfAttention(nn.Module):
         att = self.attn_drop(att)
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         #y = torch.einsum('ijkl,ijlm->ijkm',[att, v]) # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        #y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        y = y.contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
         # output projection
         y = self.resid_drop(self.proj(y))
